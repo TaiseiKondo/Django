55aa7fae7a8763d649570fba53ee52f395f4769f
TaiseiKondo <baritech.kondo@gmail.com>
2023-06-12 16:23:11 +0900

    My Django Girls app, first commit

---
+++ django_tutorial/python_intro.py
@@ -0,0 +1,16 @@
+a = 3
+b = 5
+
+if a > b:
+    print(f"{a} is indeed greater than {b}")
+else:
+    print(f"{a} is not greater than {b}")
+
+
+name = 'Taisei'
+def hi():
+  print('Hello,' + name + '!')
+  print('How are you?')
+
+hi()
+  
